b14923d7444a9abf5792e5a6e82284e90bf06b9d
Oleg Chepelev <yorkblansh@gmail.com>
2022-05-30 12:10:47 +0300

    update

--- src/ROUTES/Auth/TextInputs/Input.auth.tsx
+++ src/ROUTES/Auth/TextInputs/Input.auth.tsx
@@ -1,16 +1,15 @@
 import { Auth__Login__Props } from 'interfaces/Auth'
 
 export default function AuthTextArea({ css, id }: Auth__Login__Props) {
-	let Name = id === 'login' ? 'login' : id === 'password' ? 'password' : '0'
-	let Type = id === 'login' ? 'text' : id === 'password' ? 'password' : '0'
+	const name = id === 'login' ? 'login' : id === 'password' ? 'password' : '0'
+	const type = id === 'login' ? 'text' : id === 'password' ? 'password' : '0'
 	return (
 		<div>
 			<input
 				id={id}
 				className={css}
 				autoComplete="off"
-				name={Name}
-				type={Type}
+				{...{ name, type }}
 				required
 				autoFocus
 			/>
