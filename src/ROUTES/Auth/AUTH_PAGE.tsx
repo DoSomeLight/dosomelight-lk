fd1ec22ea47856ea09492a53f62a42259cf7fd2c
Oleg Chepelev <yorkblansh@gmail.com>
2022-06-03 18:42:41 +0300

    🌈 style(AUTH_PAGE): code format

--- src/ROUTES/Auth/AUTH_PAGE.tsx
+++ src/ROUTES/Auth/AUTH_PAGE.tsx
@@ -6,17 +6,23 @@ export const AUTH_PAGE = () => (
 			<div className="wrapper-left">
 				<h3 className="sign-in">Авторизация</h3>
 			</div>
+
 			<div className="clear"></div>
+
 			<div className="div_in">
 				<input type="text" placeholder="Имя" />
 			</div>
+
 			<div className="div_in">
 				<input type="password" placeholder="Пароль" />
 			</div>
+
 			<div className="sign_in"> Вход</div>
+
 			<div className="wrapper-right">
 				<div className="registr"> Регистрация</div>
 			</div>
+
 			<div className="clear"></div>
 		</form>
 	</div>
