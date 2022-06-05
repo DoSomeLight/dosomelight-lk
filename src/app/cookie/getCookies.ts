7b611a20a2c42d54d40144dd7902e009e765a956
Oleg Chepelev <yorkblansh@gmail.com>
2022-06-05 21:38:25 +0300

    tt

--- src/app/cookie/getCookies.ts
+++ src/app/cookie/getCookies.ts
@@ -3,7 +3,7 @@ import cookie from 'cookie'
 
 export const getCookies = (): COOKIES_DATA | false => {
 	// console.dir(cookie.parse(document.cookie))
-	if (cookie.parse(document.cookie)) {
+	if (document.cookie) {
 		return JSON.parse(cookie.parse(document.cookie).lk_data)
 	} else return false
 }
