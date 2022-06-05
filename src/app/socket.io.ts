2fa3c79963b8f937d91549cf80cfa35850111425
Oleg Chepelev <yorkblansh@gmail.com>
2022-06-05 23:01:44 +0300

    update socketio

--- src/app/socket.io.ts
+++ src/app/socket.io.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-export const socket = io('https://my.dosomelight.com', {
+export const socket = io('https://my.dosomelight.ru', {
 	query: { aatthh: '123' },
 	//   autoConnect: true,
 	//   reconnection: true,
