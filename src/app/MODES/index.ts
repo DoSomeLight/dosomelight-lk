222d4df480a3087a1eddcdb43be7dd01b77770f8
Oleg Chepelev <yorkblansh@gmail.com>
2022-06-05 21:28:09 +0300

    test

--- src/app/MODES/index.ts
+++ src/app/MODES/index.ts
@@ -5,7 +5,7 @@ import { _PRODUCTION_MODE as _PROD_cookie_MODE } from './_PROD_MODE'
 export const NODE_ENV = process.env.NODE_ENV,
 	PRODUCTION = NODE_ENV === 'production'
 
-export const get_COOKIE_DATA = (): COOKIES_DATA =>
-	PRODUCTION ? (_PROD_cookie_MODE() ? _PROD_cookie_MODE() : _DEV_cookie_MODE()) : _DEV_cookie_MODE()
+export const get_COOKIE_DATA = (): COOKIES_DATA => _DEV_cookie_MODE()
+// PRODUCTION ? (_PROD_cookie_MODE() ? _PROD_cookie_MODE() : _DEV_cookie_MODE()) : _DEV_cookie_MODE()
 
 //(PRODUCTION ? _PROD_cookie_MODE() : _DEV_cookie_MODE())
