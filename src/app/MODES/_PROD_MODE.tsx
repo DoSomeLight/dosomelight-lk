bbd62f177bf8e00e468a8b99497779e63f8cb030
Oleg Chepelev <yorkblansh@gmail.com>
2022-06-05 21:36:07 +0300

    tt

--- src/app/MODES/_PROD_MODE.tsx
+++ src/app/MODES/_PROD_MODE.tsx
@@ -2,9 +2,12 @@ import { recogUser } from 'app/LK/services/fns/recogUser'
 import { getCookies } from 'app/cookie/getCookies'
 import { socket } from 'app/socket.io'
 import { COOKIES_DATA } from 'app/vars-ts'
+import { _DEV_cookie_MODE } from './_DEV_MODE'
 
 export const _PRODUCTION_MODE = (): COOKIES_DATA => {
 	const cookies_data = getCookies()
-	socket.on('connect', () => recogUser(cookies_data))
-	return cookies_data
+	if (cookies_data) {
+		socket.on('connect', () => recogUser(cookies_data))
+		return cookies_data
+	} else _DEV_cookie_MODE()
 }
--- src/app/cookie/getCookies.ts
+++ src/app/cookie/getCookies.ts
@@ -1,7 +1,9 @@
 import { COOKIES_DATA } from 'app/vars-ts'
 import cookie from 'cookie'
 
-export const getCookies = (): COOKIES_DATA => {
-	console.dir(cookie.parse(document.cookie))
-	return JSON.parse(cookie.parse(document.cookie).lk_data)
+export const getCookies = (): COOKIES_DATA | false => {
+	// console.dir(cookie.parse(document.cookie))
+	if (cookie.parse(document.cookie)) {
+		return JSON.parse(cookie.parse(document.cookie).lk_data)
+	} else return false
 }
